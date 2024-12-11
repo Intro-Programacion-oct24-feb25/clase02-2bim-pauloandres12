7b261d5697a857f04c749fc0b9a65df1cb555696
Paulo Andres <pbustamante@utpl.edu.ec>
2024-12-11 12:42:38 -0500

    cambios 4

--- Ejemplos/src/paquete2/Ejemplo08.java
+++ Ejemplos/src/paquete2/Ejemplo08.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package paquete1;
+package paquete2;
 
 import java.util.Scanner;
 
@@ -26,22 +26,35 @@ public class Ejemplo08 {
         // arreglo de tipo entero
         // se le asigna valores concretos al arreglo cuando se lo
         // inicializa
-        int[] arreglo1 = {1000, 2000, 3000, 4000, 5000}; // arreglo de 5 elementos
+        int[] arreglo1 = {1001, 2001, 3000, 4003, 5000}; // arreglo de 5 elementos
+        int[] sumas = new int[2];
         
         // Se hace uso del ciclo repetitivo for para recorrer el arreglo
         for (int i = 0; i < arreglo1.length; i++) {
+            if(arreglo1[i]%2 == 0){
+                sumas[0] = sumas[0] + arreglo1[i];
+            }else{
+                sumas[1] = sumas[1] + arreglo1[i];
+            }
             // agrego el valor de la posición del arreglo a la variable
             // acumuladora  suma
             suma = suma + arreglo1[i];
             
             // acumula mi cadenaFinal
-            cadenaFinal = String.format("%s%d\n", cadenaFinal, arreglo1[i]);
+            cadenaFinal = String.format("%s%d\n", 
+                    cadenaFinal, 
+                    arreglo1[i]);
         }
         
         // System.out.printf("La suma de los valores del arreglo es: %d\n", suma);
         // cadenaFinal = String.format("%s%d\n", cadenaFinal, suma);
-        System.out.printf("%s\nLa suma de los valores del arreglo es: %d\n", 
-                cadenaFinal, suma);
+        System.out.printf("%s\nLa suma de los valores del arreglo es: %d\n"
+                + "La suma de pares es: %d\n"
+                + "La suma de impares es: %d\n", 
+                cadenaFinal, 
+                suma,
+                sumas[0],
+                sumas[1]);
         
     }
     /*
