50aa8506e10ebf5aa46163516838030470bb5981
Paulo Andres <pbustamante@utpl.edu.ec>
2024-12-11 12:25:37 -0500

    cambios 3

--- Ejemplos/src/paquete2/Ejemplo062.java
+++ Ejemplos/src/paquete2/Ejemplo062.java
@@ -3,15 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package paquete1;
+package paquete2;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
  *
  * @author reroes
  */
-public class Ejemplo061 {
+public class Ejemplo062 {
 
     /**
      * @param args the command line arguments
@@ -19,23 +20,26 @@ public class Ejemplo061 {
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
         Scanner entrada = new Scanner(System.in);
-        String dato;
+        entrada.useLocale(Locale.US);
+        double dato;
         // arreglo de tipo entero
-        String[] arreglo1 = new String[5]; //  se guardaran nombres de personas
+        double[] arreglo1 = new double[7];
         
         // Se asigna valores al arreglo, pidiendo valores por teclado
         // se usa un ciclo repetitivo
         for (int i = 0; i < arreglo1.length; i++) {
+        
             System.out.println("Ingrese un valor para el arreglo");
-            dato = entrada.nextLine();
+            dato = entrada.nextDouble();
             // se asigna la variable dato a la posición del arreglo
             // de acuerdo al valor de i
             arreglo1[i] = dato;
+            
         }
         
         // Se hace uso del ciclo repetitivo for para recorrer el arreglo
         for (int i = 0; i < arreglo1.length; i++) {
-            System.out.printf("Subíndice o Índice %d, con valor: %s\n",
+            System.out.printf("Subíndice o Índice %d, con valor %.2f\n",
                     i, arreglo1[i]);
         }
         
